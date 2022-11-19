4d338cd40fa243ef05ff6e7f75c27f4216fe858f
Arun Kumar <arunkumar0205ak@gmail.com>
2022-11-19 08:51:17 +0530

    FirstAutomationTest.py

--- LearningSelenium/FirstAutomationTest.py
+++ LearningSelenium/FirstAutomationTest.py
@@ -1,5 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.firefox.service import Service
 
-driver = webdriver.Chrome(executable_path="C:\\browserdrivers\\chromedriver.exe")
+chromeser = Service("C:\\browserdrivers\\chromedriver.exe")
+edgeser = Service("C:\\browserdrivers\\msedgedriver.exe")
+#fireser = Service("C:\\browserdrivers\\geckodriver.exe")
+foxser = Service("C:\browserdrivers\geckodriver.exe")
 
-driver.get("https://www.rcvacadmey.com")
+driver = webdriver.Chrome(service=chromeser)
+egdriver = webdriver.Edge(service=edgeser)
+#firedriver = webdriver.Firefox(service=fireser)
+firedriver = webdriver.Firefox(service=foxser)
+
+driver.get("https://www.rcvacademy.com")
+egdriver.get("https://www.rcvacademy.com")
+firedriver.get("https://www.rcvacademy.com")
+
+driver.maximize_window()
+egdriver.maximize_window()
+firedriver.maximize_window()
+
+driver.minimize_window()
+egdriver.minimize_window()
+firedriver.minimize_window()
+
+print(driver.title)
+print(egdriver.title)
+print(firedriver.title)
+
+driver.close()
+egdriver.close()
+firedriver.close()
